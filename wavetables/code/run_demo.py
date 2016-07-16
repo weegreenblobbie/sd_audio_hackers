103515dfd4f05cc3e4cdcf0c80e82937b7069f09
Nick Hilton <weegreenblobbie@yahoo.com>
2016-07-15 20:40:22 -0700

    testing phase parameter, renamed module

--- wavetables/code/run_demo.py
+++ wavetables/code/run_demo.py
@@ -10,34 +10,45 @@ from callables import Circular
 
 import draw
 
-import wave_table_gen
+import wt_oscillators
 
 
 def main():
 
     sr = 1000
-
-    freq = Constant(3.0)
-    phase = Constant(0.0)
+    dur = 1.0
 
     #--------------------------------------------------------------------------
     # drawn waveforms
 
-    x = draw.sine(sr, 1.0, freq)
+    _3_hz = Constant(3)
+
+    x = draw.sine(sr, dur, _3_hz)
 
     plt.figure()
     plt.plot(x)
     plt.grid(True)
-    plt.title('Sine wave: f = %d Hz' % freq())
+    plt.title('Sine wave: f = %d Hz' % _3_hz())
 
-    f_3_10 = draw.line(sr, 1.0, 3, 10)
+    f_3_10 = Circular(draw.line(sr, dur, 3, 10))
 
-    x = draw.sine(sr, 1.0, Circular(f_3_10))
+    x = draw.sine(sr, dur, f_3_10)
 
     plt.figure()
     plt.plot(x)
     plt.grid(True)
     plt.title('Sine wave: f = 3-10 Hz')
+
+    phase = Circular(draw.line(sr, dur, 0.0, 1.0))
+
+    x = draw.sine(sr, dur, _3_hz, phase)
+
+    plt.figure()
+    plt.plot(x)
+    plt.grid(True)
+    plt.title('Sine wave: f = 3, phase = 0 - 180 deg')
+
+    plt.show()
 
     #--------------------------------------------------------------------------
     # wave table gen Nearest
@@ -50,28 +61,25 @@ def main():
     table_500  = draw.sine( 500,  500, _1_hz)
     table_250  = draw.sine( 250,  250, _1_hz)
 
-    gen_1000 = wave_table_gen.Nearest(1000, table_1000)
-    gen_500  = wave_table_gen.Nearest(1000, table_500)
-    gen_250  = wave_table_gen.Nearest(1000, table_250)
+    gen_1000 = wt_oscillators.Nearest(sr, table_1000)
+    gen_500  = wt_oscillators.Nearest(sr, table_500)
+    gen_250  = wt_oscillators.Nearest(sr, table_250)
 
     dur = 1.0
 
-    x0 = gen_1000.generate(dur, Circular(f_3_10))
-    x1 = gen_500.generate(dur,  Circular(f_3_10))
-    x2 = gen_250.generate(dur,  Circular(f_3_10))
+    x0 = gen_1000.generate(dur, f_3_10)
+    x1 = gen_500.generate(dur,  f_3_10)
+    x2 = gen_250.generate(dur,  f_3_10)
 
     plt.figure()
     plt.plot(x0, 'b-', label = 'wt 1000')
     plt.plot(x1, 'r-', label = 'wt 500')
     plt.plot(x2, 'm-', label = 'wt 250')
-    plt.title('wave_table_gen.Neartest signals')
+    plt.title('wt_oscillators.Neartest signals')
     plt.grid(True)
     plt.legend()
 
     # round off error residuals
-
-    # res_500 = np.abs(x1 - x0)
-    # res_250 = np.abs(x2 - x0)
 
     res_500 = x1 - x0
     res_250 = x2 - x0
@@ -79,33 +87,32 @@ def main():
     plt.figure()
     plt.plot(res_500, label = 'wt 500 error')
     plt.plot(res_250, label = 'wt 250 error')
-    plt.title('wave_table_gen.Nearest residual error')
+    plt.title('wt_oscillators.Nearest residual error')
     plt.grid(True)
     plt.legend()
+
+    plt.show()
 
     #--------------------------------------------------------------------------
     # wave table gen Lininterp
 
-    gen_1000 = wave_table_gen.Lininterp(1000, table_1000)
-    gen_500  = wave_table_gen.Lininterp(1000, table_500)
-    gen_250  = wave_table_gen.Lininterp(1000, table_250)
+    gen_1000 = wt_oscillators.Lininterp(sr, table_1000)
+    gen_500  = wt_oscillators.Lininterp(sr, table_500)
+    gen_250  = wt_oscillators.Lininterp(sr, table_250)
 
-    x0 = gen_1000.generate(dur, Circular(f_3_10))
-    x1 = gen_500.generate(dur,  Circular(f_3_10))
-    x2 = gen_250.generate(dur,  Circular(f_3_10))
+    x0 = gen_1000.generate(dur, f_3_10)
+    x1 = gen_500.generate(dur,  f_3_10)
+    x2 = gen_250.generate(dur,  f_3_10)
 
     plt.figure()
     plt.plot(x0, 'b-', label = 'wt 1000')
     plt.plot(x1, 'r-', label = 'wt 500')
     plt.plot(x2, 'm-', label = 'wt 250')
-    plt.title('wave_table_gen.Lininterp signals')
+    plt.title('wt_oscillators.Lininterp signals')
     plt.grid(True)
     plt.legend()
 
     # round off error residuals
-
-    # res_500 = np.abs(x1 - x0)
-    # res_250 = np.abs(x2 - x0)
 
     res_500 = x1 - x0
     res_250 = x2 - x0
@@ -113,10 +120,36 @@ def main():
     plt.figure()
     plt.plot(res_500, label = 'wt 500 error')
     plt.plot(res_250, label = 'wt 250 error')
-    plt.title('wave_table_gen.Lininterp residual error')
+    plt.title('wt_oscillators.Lininterp residual error')
     plt.grid(True)
     plt.legend()
 
+    plt.show()
+
+    #--------------------------------------------------------------------------
+    # draw with phase
+
+    phase = Circular(draw.line(sr, 1.0, 0.0, 1.0))
+
+    _3_hz = Constant(3.0)
+
+    x0 = draw.sine(sr, dur, _3_hz, phase)
+    x1 = gen_250.generate(dur, _3_hz, phase)
+
+    plt.figure()
+    plt.plot(x0, label = 'drawn')
+    plt.plot(x1, label = 'wt 250 interp')
+    plt.title('3 Hz sine with 180 deg phase change')
+    plt.grid(True)
+    plt.legend()
+
+    res = x1 - x0
+
+    plt.figure()
+    plt.plot(res, label = 'wt 250 interp error')
+    plt.title('Residual error with 180 deg phase change')
+    plt.grid(True)
+    plt.legend()
 
     plt.show()
 
