454b532d0a94821f45393d0d30f4db624e4b47f5
Nick Hilton <weegreenblobbie@yahoo.com>
2016-07-15 20:19:13 -0700

    Added lin interpolation wave table generator

--- wavetables/code/run_demo.py
+++ wavetables/code/run_demo.py
@@ -30,9 +30,9 @@ def main():
     plt.grid(True)
     plt.title('Sine wave: f = %d Hz' % freq())
 
-    f = draw.line(sr, 1.0, 3, 10)
+    f_3_10 = draw.line(sr, 1.0, 3, 10)
 
-    x = draw.sine(sr, 1.0, Circular(f))
+    x = draw.sine(sr, 1.0, Circular(f_3_10))
 
     plt.figure()
     plt.plot(x)
@@ -40,7 +40,7 @@ def main():
     plt.title('Sine wave: f = 3-10 Hz')
 
     #--------------------------------------------------------------------------
-    # wave table generators
+    # wave table gen Nearest
 
     # create a 3 tables each holding 1 cycle
 
@@ -55,17 +55,16 @@ def main():
     gen_250  = wave_table_gen.Nearest(1000, table_250)
 
     dur = 1.0
-    _3_hz = Constant(3.0)
 
-    x0 = gen_1000.generate(dur, _3_hz)
-    x1 = gen_500.generate(dur, _3_hz)
-    x2 = gen_250.generate(dur, _3_hz)
+    x0 = gen_1000.generate(dur, Circular(f_3_10))
+    x1 = gen_500.generate(dur,  Circular(f_3_10))
+    x2 = gen_250.generate(dur,  Circular(f_3_10))
 
     plt.figure()
     plt.plot(x0, 'b-', label = 'wt 1000')
     plt.plot(x1, 'r-', label = 'wt 500')
     plt.plot(x2, 'm-', label = 'wt 250')
-    plt.title('Wave table signals')
+    plt.title('wave_table_gen.Neartest signals')
     plt.grid(True)
     plt.legend()
 
@@ -83,6 +82,41 @@ def main():
     plt.title('wave_table_gen.Nearest residual error')
     plt.grid(True)
     plt.legend()
+
+    #--------------------------------------------------------------------------
+    # wave table gen Lininterp
+
+    gen_1000 = wave_table_gen.Lininterp(1000, table_1000)
+    gen_500  = wave_table_gen.Lininterp(1000, table_500)
+    gen_250  = wave_table_gen.Lininterp(1000, table_250)
+
+    x0 = gen_1000.generate(dur, Circular(f_3_10))
+    x1 = gen_500.generate(dur,  Circular(f_3_10))
+    x2 = gen_250.generate(dur,  Circular(f_3_10))
+
+    plt.figure()
+    plt.plot(x0, 'b-', label = 'wt 1000')
+    plt.plot(x1, 'r-', label = 'wt 500')
+    plt.plot(x2, 'm-', label = 'wt 250')
+    plt.title('wave_table_gen.Lininterp signals')
+    plt.grid(True)
+    plt.legend()
+
+    # round off error residuals
+
+    # res_500 = np.abs(x1 - x0)
+    # res_250 = np.abs(x2 - x0)
+
+    res_500 = x1 - x0
+    res_250 = x2 - x0
+
+    plt.figure()
+    plt.plot(res_500, label = 'wt 500 error')
+    plt.plot(res_250, label = 'wt 250 error')
+    plt.title('wave_table_gen.Lininterp residual error')
+    plt.grid(True)
+    plt.legend()
+
 
     plt.show()
 
--- wavetables/code/wave_table_gen.py
+++ wavetables/code/wave_table_gen.py
@@ -56,7 +56,7 @@ class Nearest(object):
         for i in range(n_samples):
 
             ph = (phase() * self._sr / 2.0) + 0.5
-            pos2 = pos + self._factor * ph
+            pos2 = self._factor * (pos + ph)
 
             # range check
 
@@ -70,7 +70,89 @@ class Nearest(object):
 
             sample = self._table[int(np.floor(pos2))]
 
-            pos += self._factor * freq()
+            pos += freq()
+
+            out[i] = sample
+
+        return out
+
+
+class Lininterp(object):
+    '''
+    Returns samples from the wavetable with linear interpolation.
+    '''
+
+    def __init__(self, sr, table):
+
+        table = np.array(table).astype(np.float32)
+
+        table = np.squeeze(table)
+
+        assert len(table.shape) == 1, 'table must be a 1D array'
+
+        # factor, if the table size != sr, the frequeny needs to be multiplied
+        # by this.
+
+        sr = np.float32(sr)
+
+        factor = np.float32(len(table)) / sr
+
+        # save into object
+
+        self._factor = factor
+        self._table = table
+        self._sr = sr
+
+
+    def generate(self, dur, freq, phase = None):
+        '''
+        gernerates a signal
+        '''
+
+        if phase is None:
+            phase = lambda : 0
+
+        assert callable(freq), "freq must be a callable object"
+        assert callable(phase), "phase must be a callable object"
+
+        n_samples = get_n_samples(self._sr, dur)
+
+        out = np.zeros(n_samples, np.float32)
+
+        pos = 0.0
+
+        for i in range(n_samples):
+
+            ph = (phase() * self._sr / 2.0) + 0.5
+            pos2 = self._factor * (pos + ph)
+
+            # range check
+
+            sr = self._factor * self._sr
+
+            while(pos2 >= sr):
+                pos2 -= sr
+
+            while(pos2 < 0):
+                pos2 += sr
+
+            i0 = np.floor(pos2)
+            i1 = i0 + 1
+
+            if i1 >= len(self._table):
+                i1 = 0
+
+            beta = pos2 - i0
+            alpha = 1.0 - beta
+
+            v0 = self._table[int(i0)]
+            v1 = self._table[int(i1)]
+
+            # weighted averate
+
+            sample = alpha * v0 + beta * v1
+
+            pos += freq()
 
             out[i] = sample
 
